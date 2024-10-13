2fc87d912579f6a900489d5864ccc84ab6284fb4
L1feed <gto200313@gmail.com>
2024-10-13 15:27:18 +0200

    deploy update

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,11 @@
+import express, { Request, Response } from "express";
+
 const PORT = 3002;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`They can hear you... (port ${PORT})`));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("server is alive");
+});
