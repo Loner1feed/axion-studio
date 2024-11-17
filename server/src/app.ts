f0ab75b93c9ae3f1c29cc7e42292d37f755d6c8b
L1feed <gto200313@gmail.com>
2024-11-17 20:48:40 +0100

    [server]: added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
 import router from "./routes";
 
 const PORT = 3002;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`They can hear you... (port ${PORT})`));
+app.use(cors());
 
 app.use("/", router);
+
+app.listen(PORT, () => console.log(`They can hear you... (port ${PORT})`));
 
 export default app;
