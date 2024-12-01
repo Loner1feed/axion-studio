8a90e16a34455048914bc0c2f34c6ecc69b6aad7
L1feed <gto200313@gmail.com>
2024-12-01 16:32:26 +0100

    [front-admin]: path aliases

--- front-admin/vite.config.ts
+++ front-admin/vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": "/src/assets",
-      "@components": "/src/components",
-      "@configs": "/src/configs",
-      "@layouts": "/src/layouts",
-      "@utils": "/src/utils",
-      "@views": "/src/utils",
+      "@assets": "/src/assets/",
+      "@components": "/src/components/",
+      "@configs": "/src/configs/",
+      "@layouts": "/src/layouts/",
+      "@utils": "/src/utils/",
+      "@views": "/src/views/",
     },
   },
 });
+
+console.log(__dirname);
