85e7d2ff515078e0ba74a32b5497a68257cb98b4
Anton Nikonov <nikonovanton66@gmail.com>
2024-12-22 02:10:44 +0100

    update main banner.

--- front/src/app/[[...language]]/page.tsx
+++ front/src/app/[[...language]]/page.tsx
@@ -5,14 +5,17 @@ import React from "react";
 import "../globals.scss";
 import { MainBanner } from "@/src/components/layout";
 import { ProjectTypes } from "@/src/components/layout/project-types/project-types";
+import { useToggleHeader } from "@/src/components/layout/header/hooks/use-toggle-header";
+import { MainPageContext } from "./page.context";
 
 export const MainPage = () => {
+  const toggleHeader = useToggleHeader();
   return (
-    <div>
+    <MainPageContext.Provider value={toggleHeader}>
       <MainBanner />
       <ProjectTypes />
       <div style={{ height: "1000px" }}></div>
-    </div>
+    </MainPageContext.Provider>
   );
 };
 
--- front/src/components/layout/project-types/project-types.tsx
+++ front/src/components/layout/project-types/project-types.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import styles from "./project-types.module.scss";
 import { Heading } from "../../common";
 import { useTranslations } from "@/src/utils/hooks";
 import { Container } from "../container/contaner";
+import { MainPageContext } from "@/src/app/[[...language]]/page.context";
 
 export const ProjectTypes = () => {
   const t = useTranslations();
+  const { revealHeaderOnScrollToElementRef } = useContext(MainPageContext);
 
   return (
-    <div className={styles.projectTypes}>
+    <div ref={revealHeaderOnScrollToElementRef} className={styles.projectTypes}>
       <Container>
         <Heading className={styles.heading}>{t.projectTypes.heading}</Heading>
       </Container>
