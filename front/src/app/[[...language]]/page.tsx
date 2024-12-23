53eebc340a15d51cfcc0c990f5823de838138b0c
Anton Nikonov <nikonovanton66@gmail.com>
2024-12-23 23:12:47 +0100

    working on benefits.

--- front/src/app/[[...language]]/page.tsx
+++ front/src/app/[[...language]]/page.tsx
@@ -7,13 +7,15 @@ import { MainBanner } from "@/src/components/layout";
 import { ProjectTypes } from "@/src/components/layout/project-types/project-types";
 import { useToggleHeader } from "@/src/components/layout/header/hooks/use-toggle-header";
 import { MainPageContext } from "./page.context";
+import { Benefits } from "@/src/components/layout/benefits/benefits";
 
 export const MainPage = () => {
   const toggleHeader = useToggleHeader();
   return (
     <MainPageContext.Provider value={toggleHeader}>
       <MainBanner />
-      <ProjectTypes />
+      {/* <ProjectTypes /> */}
+      <Benefits />
       <div style={{ height: "1000px" }}></div>
     </MainPageContext.Provider>
   );
