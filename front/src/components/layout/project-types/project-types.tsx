189ea6389bf8c9a2031cc747f3133da8f74104ff
L1feed <gto200313@gmail.com>
2024-12-24 17:47:29 +0100

    [front]: project types heading related changes

--- front/src/components/layout/project-types/project-types.tsx
+++ front/src/components/layout/project-types/project-types.tsx
@@ -1,30 +1,24 @@
 "use client";
 
 import React from "react";
+import { ProjectType } from "@/src/utils/types";
+import { useTranslations } from "@/src/utils/hooks";
+import { Heading, FadeInOnView } from "@/src/components/common";
+import { Container } from "../container/contaner";
+import { ProjectGrid } from "./common/project-grid";
 
 import styles from "./project-types.module.scss";
-import { Heading } from "../../common";
-import { useTranslations } from "@/src/utils/hooks";
-import { Container } from "../container/contaner";
-import { FadeInOnView } from "../../common";
-import { ProjectGrid } from "./common/project-grid";
-import { ProjectType } from "@/src/utils/types";
 
 interface ProjectTypesProps {
   data: ProjectType[];
 }
-// import { MainPageContext } from "@/src/app/[[...language]]/page.context";
 
 export const ProjectTypes: React.FC<ProjectTypesProps> = ({ data }) => {
   const t = useTranslations();
-  // const { revealHeaderOnScrollToElementRef } = useContext(MainPageContext);
 
   return (
     <FadeInOnView>
-      <div
-        // ref={revealHeaderOnScrollToElementRef}
-        className={styles.projectTypes}
-      >
+      <div className={styles.projectTypes}>
         <Container>
           <Heading className={styles.heading}>{t.projectTypes.heading}</Heading>
           <ProjectGrid data={data} />
@@ -33,16 +27,3 @@ export const ProjectTypes: React.FC<ProjectTypesProps> = ({ data }) => {
     </FadeInOnView>
   );
 };
-
-// export const ProjectTypes: React.FC = () => {
-//   return (
-//     <FadeInOnView>
-//       <div className={styles.projectTypes}>
-//         <Container>
-//           <h1></h1>
-//           <ProjectGrid data={mockProjectsData} />
-//         </Container>
-//       </div>
-//     </FadeInOnView>
-//   );
-// };
