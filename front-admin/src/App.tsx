2703f5a4282db63823636c75c090a97d5a9bab9d
Loner1feed <gto200313@gmail.com>
2025-03-09 19:22:04 +0100

    fix

--- front-admin/src/App.tsx
+++ front-admin/src/App.tsx
@@ -2,7 +2,7 @@ import { Router } from "@components/common";
 import "./App.css";
 
 function App() {
-  console.log(process.env.VITE_API_URL);
+  console.log("api: ", import.meta.env.VITE_API_URL);
   return <Router />;
 }
 
